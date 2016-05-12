a7053b5f1343c97dda1657e997a51da4b0d07051
sukesh515 <sukesh515@yahoo.co.in>
2016-05-12 14:20:28 +0100

    ALL objects identified and can book online

--- LogBetter/src/BetterLogin.java
+++ LogBetter/src/BetterLogin.java
@@ -1,6 +1,9 @@
 
+import java.util.Iterator;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.NoSuchElementException;
@@ -48,7 +51,10 @@ public class BetterLogin {
 		 driver.findElement(By.id("login_Email")).sendKeys("sukesh515@gmail.com");
 		 driver.findElement(By.id("login_Password")).sendKeys("Hyundai515");
 		 driver.findElement(By.id("login")).click();
+				 		 
 		 driver.findElement(By.linkText("Make a Booking")).click();
+		 
+	
 		
 		 // Avoneil is unchecked and then checked
 		 
@@ -56,20 +62,17 @@ public class BetterLogin {
 					 
 		 driver.findElement(By.xpath(".//*[@id='contentWrapper']/div[2]/div/div[2]/div[1]/form/div[3]/label/input")).click();
 		 
-              Thread.sleep(120);
+           Thread.sleep(120);
               
-			 // Other activities is checked 
-              Actions act = new Actions(driver);
-              System.out.println("Double clicked on otheractivities ");
-              act.moveToElement(driver.findElement(By.xpath(".//*[@id='behaviours']/div[2]/label"))).doubleClick().build().perform();
-              
-               // driver.findElement(By.xpath(".//*[@id='behaviours']/div[2]/label")).click();
+			 
+         driver.findElement(By.xpath(".//*[@id='behaviours']/div[2]/label")).click();
               System.out.println("Sleeping for 320S ");
 		   // Again other activities is checked since Badminton is not visible
-		    Thread.sleep(320L);
+		    Thread.sleep(3220L);
 		    System.out.println("Double clicked again 2nd time otheractivities ");
-		     act.moveToElement(driver.findElement(By.xpath(".//*[@id='behaviours']/div[2]/label"))).doubleClick().build().perform();
-		  //driver.findElement(By.xpath(".//*[@id='behaviours']/div[2]/label")).click();
+		    // act.moveToElement(driver.findElement(By.xpath(".//*[@id='behaviours']/div[2]/label"))).doubleClick().build().perform();
+	
+		    driver.findElement(By.xpath(".//*[@id='behaviours']/div[2]/label")).click();
 		  
 		 // Badminton checkbox is checked 
 		     System.out.println("Checking Badminton ");
@@ -85,14 +88,42 @@ public class BetterLogin {
 		  
 		driver.findElement(By.xpath(".//*[@id='bottomsubmit']")).click();
 		
-		driver.close();
+		Thread.sleep(3220L);
 		
-		/*if (count ==0)
 			
-		{
-			System.out.println("Verified Element is not found ");
-		}
-			*/
+		
+		driver.switchTo().frame("TB_iframeContent");
+		
+		driver.findElement(By.xpath(".//*[@id='slot250503']")).click();
+		
+		Thread.sleep(3220L);
+		// driver.switchTo().window(".//*[@id='TB_window']");
+		
+		System.out.println("Add to Basket Popup window ");
+		
+		driver.switchTo().frame("TB_iframeContent");
+		
+		 //driver.findElement(By.xpath(".//*[@id='cmbAvailableCourts']")).click();
+		
+		
+		driver.findElement(By.linkText("Add to Basket")).click();
+		
+		Thread.sleep(3220L);
+		
+		driver.navigate().refresh();
+		
+		driver.findElement(By.xpath(".//*[@id='submenu']/ul/li[12]/a")).click();
+			
+		
+		Thread.sleep(3220L);
+		
+		System.out.println("This will complete the Booking ");
+		
+		driver.findElement(By.linkText("Complete")).click();
+		driver.findElement(By.linkText("Continue")).click();
+		
+		
+		//driver.close();
 		  
 	  
 
