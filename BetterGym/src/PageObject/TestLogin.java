5d2b77c8132591a3eece2ac32a9d53b88484b69d
sukesh515 <sukesh515@yahoo.co.in>
2016-05-17 15:38:13 +0100

    Added Properties file and methods

--- BetterGym/src/PageObject/TestLogin.java
+++ BetterGym/src/PageObject/TestLogin.java
@@ -1,9 +1,12 @@
 package PageObject;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
-
 
 
 public class TestLogin {
@@ -11,16 +14,25 @@ public class TestLogin {
 	
 	@Test
 	
-	public void LoginBet() throws InterruptedException {
+	public void LoginBet() throws InterruptedException, IOException {
 		
 		WebDriver driver = new FirefoxDriver();
 		
+		Properties prop = new Properties();
+		
+		 FileInputStream fil = new FileInputStream("C:\\Users\\IBM_ADMIN\\git\\BetterSelenium\\LogBetter\\src\\datadriven.properties");
+		
+		//FileInputStream fil = new FileInputStream("datadriven.properties");
+		
+		
+		prop.load(fil);
+
 		driver.get("https://better.legendonlineservices.co.uk/enterprise/account/login");
 		
 		LoginPageBetter lp = new LoginPageBetter(driver);
 		
-		lp.username().sendKeys("sukesh515@gmail.com");
-		lp.Password().sendKeys("Hyundai515");
+		lp.username().sendKeys(prop.getProperty("username"));
+		lp.Password().sendKeys(prop.getProperty("password"));
 		
 		lp.Login().click();
 		Thread.sleep(3220L);
